d9dfb3c69719d542a7e0d2aa34a3f2263b2fcc6d
Dazai-LC <ly0389383371@gmail.com>
2026-05-16 22:44:15 +0700

    setup firebase realtime database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    // Thêm dòng này ngay bên dưới các plugin có sẵn
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.nightlife_finder"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.nightlife_finder"
-        minSdk = 36
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -25,12 +20,14 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,21 +35,25 @@ android {
 }
 
 dependencies {
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    // Import Firebase Bill of Materials (BoM) để tự động quản lý phiên bản tương thích
+
+    // Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
 
-    // Các module Firebase theo đúng Scope đồ án
+    // Firebase
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-database")
 
-    // Thư viện Glide hỗ trợ load ảnh từ URL về app mượt mà
+    // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
